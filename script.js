37f416e89253f934b173a76f573e645127c17a7e
Константин <shkretov97@mail.ru>
2025-12-04 15:22:46 +0500

    fix

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let buttonText; 
+let buttonText;
 document.querySelector(".prizes__btn").addEventListener("click", function () {
-  document.querySelector(".prizes__info").classList.toggle("prizes__info--active");
+  document
+    .querySelector(".prizes__info")
+    .classList.toggle("prizes__info--active");
   buttonText = this.textContent;
   this.textContent = this.dataset.text;
-  this.dataset.text = buttonText
+  this.dataset.text = buttonText;
 });
